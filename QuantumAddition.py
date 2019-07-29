635147cd7c55def4e9c9f5f14ee2dd19637fa9a1
Lebin Yu <lebin.yuchn@gmail.com>
2019-07-29 22:07:34 +0200

    add annotations

--- QuantumAddition.py
+++ QuantumAddition.py
@@ -13,11 +13,14 @@ from mpi4py import MPI
 #eng = HiQMainEngine(engine_list=get_engine_list()+[CommandPrinter()])
 eng = HiQMainEngine(engine_list=get_engine_list())
 
+# number of digits to two sets of qureg
 dig_of_qureg = 3
 
+# two qureg of same digits,|unit,tens,hendreds,...>
 qureg1 = eng.allocate_qureg(dig_of_qureg)
 qureg2 = eng.allocate_qureg(dig_of_qureg)
 
+# quantum fourier transform for qureg
 def quantum_fourier(qureg):
     for i in range(len(qureg)):
         H | qureg[-1 - i]
@@ -25,7 +28,7 @@ def quantum_fourier(qureg):
             with Control(eng, qureg[-1 - (j + i + 1)]):
                 R(np.pi / (1 << (1 + j))) | qureg[-1 - i]
 
-#add qureg1 to transformed qureg2
+# add qureg1 to transformed qureg2
 def quantum_addition(qureg1,qureg2):
     for i in range(len(qureg1)):
         for j in range(len(qureg1) - i):
@@ -34,23 +37,32 @@ def quantum_addition(qureg1,qureg2):
 
 
 
-
+# initialize quregs, take |110> + |100> for example
+# the output should be |001> in this case
+X | qureg1[0]
 X | qureg1[1]
-#X | qureg1[1]
-X | qureg2[1]
+X | qureg2[0]
 
+# do QFT to qureg2
 with Compute(eng):
     quantum_fourier(qureg2)
-    
+
+# do quantum addition
 quantum_addition(qureg1,qureg2)
+
+# reverse QFT to qureg2
 Uncompute(eng)
 
 
 eng.flush()
+
+# cheating wavefunction
 mapping, wave_func = copy.deepcopy(eng.backend.cheat())
+
+# print the possibility of the result state for cheating
 print(eng.backend.get_probability("001", qureg2))
 
-# Measure the qubit with a basis spanned by {|0>, |1>}
+# measure the qubit with a basis spanned by {|0>, |1>}
 All(Measure) | qureg2
 
 
@@ -60,4 +72,4 @@ All(Measure) | qureg2
 
  # Obtain the output. Note that the result is still stored in the qubit object yet clashed into a classical bit
 print("Measured: {}".format([int(qubit) for qubit in qureg2]))
-#print(gate_set)
+
