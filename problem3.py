7c3b621161c8a30a63bc8aab5f54daa6bdf18848
BoxiLi <etamin1201@gmail.com>
2019-07-31 10:35:21 +0200

    problem3 start to try random

--- problem3.py
+++ problem3.py
@@ -1,131 +1,115 @@
-"""
-An example of decomposing controlled rotation gate into CX, CZ and single rotation
-"""
-import copy
-from copy import deepcopy
-import projectq
-import numpy as np
-from projectq.backends import CommandPrinter
-from projectq.ops import Allocate, Deallocate, Measure, All, H, X, Y, Z, H, S, T, CX, CZ, Rx, Ry, Rz, Ph
+import math
+from projectq import MainEngine
+from projectq.ops import X, Y, Z, H, S, T, CX, CZ, Rx, Ry, Rz, Measure, All
 from projectq.meta import Loop, Compute, Uncompute, Control
-from projectq.setups import restrictedgateset
 from projectq.cengines import (MainEngine,
-                               AutoReplacer,
-                               LocalOptimizer,
-                               TagRemover,
-                               DecompositionRuleSet)
-from hiq.projectq.cengines import GreedyScheduler, HiQMainEngine
-from hiq.projectq.backends import SimulatorMPI
+AutoReplacer,
+LocalOptimizer,
+TagRemover,
+InstructionFilter,
+DecompositionRuleSet)
 import projectq.setups.decompositions
-import scipy.optimize
-
+from hiq.projectq.backends import SimulatorMPI
+from hiq.projectq.cengines import GreedyScheduler, HiQMainEngine
 from mpi4py import MPI
-
-backend = SimulatorMPI(gate_fusion=True, num_local_qubits=20)
-
-cache_depth = 10
-rule_set = DecompositionRuleSet(modules=[projectq.setups.decompositions])
-engines = [TagRemover()
-            , LocalOptimizer(cache_depth)
-            , AutoReplacer(rule_set)
-            , TagRemover()
-            , LocalOptimizer(cache_depth)
-            , GreedyScheduler()
-            ]
-
-theta1=np.pi/2
-theta2=np.pi/2
-theta3=np.pi/2
-theta4=0
-theta5=0
+from circuit import run_circuit
 
 
+################################################
+import numpy as np
+from scipy.optimize import minimize
+############################################
 
-# eng = HiQMainEngine(backend, engine_list=[CommandPrinter()] + engines + [CommandPrinter()])
-# eng = HiQMainEngine(backend, engine_list= engines)
-# Qureg = eng.allocate_qureg(14)
+
+# Function that need to be implemented by the contestants
+def calculate_theta(eng, final_state):
+    """
+    Calculate the optimal theta value.
+    Args:
+    eng (MainEngine): Main compiler engine to use.
+    final_state (list): list of quantum bit states.
+    Returns:
+    list of best theta values.
+    """
+
+    return [0, 0, 0, 0, 0]
 
 
-from circuit import example_circuit
-# para = np.array([theta1, theta2, theta3, theta4, theta5], dtype = float)
-para = (theta1, theta2, theta3, theta4, theta5)
-def measure_amplitude(para):
-    eng = HiQMainEngine(backend, engine_list= engines)
-    Qureg = eng.allocate_qureg(14)
-    # mapping, wave_func = copy.deepcopy(eng.backend.cheat())
-    example_circuit(Qureg, para)
-    All(Measure) | Qureg
+
+def list_to_str(final_state):
+    [str(i) for i in final_state]
+    str_state = "".join([str(i) for i in final_state])
+    return str_state
+
+# ###########################
+# # Test for list_to_str
+# final_state = [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1]
+# str_state = list_to_str(final_state)
+# print(str_state)
+# ###########################
+
+
+
+if __name__ == "__main__":
+    # use projectq simulator
+    #eng = MainEngine()
+    # use hiq simulator
+    # TODO carefull with num_local_qubits
+    backend = SimulatorMPI(gate_fusion=True, num_local_qubits=15)
+    cache_depth = 10
+    rule_set = DecompositionRuleSet(modules=[projectq.setups.decompositions])
+    engines = [TagRemover()
+                , LocalOptimizer(cache_depth)
+                , AutoReplacer(rule_set)
+                , TagRemover()
+                , LocalOptimizer(cache_depth)
+                , GreedyScheduler()
+                ]
+    # make the compiler and run the circuit on the simulator backend
+    eng = HiQMainEngine(backend, engine_list = engines)
+    qureg = eng.allocate_qureg(14)
+    # Just an example, you need to design more final state cases for testing..
+    final_state = [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1]
+    # Function that need to be implemented by the contestants
+#     theta = calculate_theta(eng, final_state)
+#     run_circuit(qureg, theta)
+#     eng.flush()
+#     print(eng.backend.get_probability(final_state, qureg))
+#     All(Measure) | qureg
+
+
+def measure_amplitude(theta, eng, qureg,  str_state):
+    from projectq.ops import Deallocate
+    run_circuit(qureg, theta)
     eng.flush()
-    amp = eng.backend.get_probability("00001111111111", Qureg)
+    amp = eng.backend.get_probability(str_state, qureg)
+    All(Measure) | qureg
 
-    # amp = eng.backend.get_probability("00", Qureg)
+    # reinitialize to |00000..00>
+    for qb in qureg:
+        if int(qb):
+            X | qb
+    eng.flush()
+
     print(1-amp)
-    # All(Deallocate) | Qureg
-    eng.flush()
-    del Qureg
+    del qureg
     if amp > 0.5:
         amp = 1
     return 1-amp
 
-measure_amplitude(para)
 
+theta = np.random.rand(5)*4*np.pi
+# measure_amplitude(eng, qureg, theta, list_to_str(final_state))
 
-print(scipy.optimize.minimize(measure_amplitude, para,
-bounds = [(-np.pi, np.pi),
-          (-np.pi, np.pi),
-          (-np.pi, np.pi),
-          (-np.pi, np.pi),
-          (-np.pi, np.pi)]
-)
-)
-
-
-
-
-# step = 1 # step size
-# precision = 0.001 #precision
-# max_iters = 2 
-
-# for i in range(max_iters):
-#     current_amp = measure_amplitude(para[0], para[1], para[2], para[3], para[4])
-#     next_amp_list = np.array([], dtype = float)
-#     next_amp_list.append(measure_amplitude(para[0] + step, para[1] + step, para[2] + step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] + step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] + step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] + step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] - step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] - step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] - step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] - step, para[3] + step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] + step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] + step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] + step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] + step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] - step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] - step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] - step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] - step, para[3] - step, para[4] + step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] + step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] + step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] + step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] + step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] - step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] - step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] - step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] - step, para[3] + step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] + step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] + step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] + step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] + step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] + step, para[2] - step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] + step, para[2] - step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] + step, para[1] - step, para[2] - step, para[3] - step, para[4] - step),
-#                         measure_amplitude(para[0] - step, para[1] - step, para[2] - step, para[3] - step, para[4] - step))
-                        
-#     for j in range(32):
-#         if next_amp_list(j) - current_amp > 0:
-#             break
-
-
-
-#example_circuit(Qureg, theta1, theta2, theta3, theta4, theta5)
+for temp in range(100):
+    amp_error = minimize(measure_amplitude, x0 = theta, args=(eng, qureg, list_to_str(final_state)),
+                bounds = [(0, 4*np.pi),
+                        (0, 4*np.pi),
+                        (0, 4*np.pi),
+                        (0, 4*np.pi),
+                        (0, 4*np.pi)],
+                method = 'L-BFGS-B'
+                )
+    print(amp_error)
+    if amp_error.fun < 0.982:
+        break
