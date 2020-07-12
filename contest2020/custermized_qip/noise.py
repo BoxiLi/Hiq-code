bc6c56292b49a3d8047f0210a424ef63ad66987d
BoxiLi <etamin1201@gmail.com>
2020-07-12 22:02:23 +0200

    fix a missing reference

--- contest2020/custermized_qip/noise.py
+++ contest2020/custermized_qip/noise.py
@@ -5,11 +5,11 @@ import numpy as np
 from numpy.random import normal
 
 from qutip.qobjevo import QobjEvo, EvoElement
-from qutip.qip.operations import expand_operator
+from .operations import expand_operator
 from qutip.qobj import Qobj
 from qutip.operators import sigmaz, destroy, identity
 from qutip.tensor import tensor
-from qutip.qip.pulse import Pulse
+from .pulse import Pulse
 
 
 __all__ = ["Noise", "DecoherenceNoise", "RelaxationNoise",
